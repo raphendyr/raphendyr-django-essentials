e3a6271238e3bc84ac458dcc752d5ade6c58e90c
Jaakko Kantojärvi <jaakko@n-1.fi>
2017-10-19 17:08:32 +0300

    Add get_random_string in CryptoExtension

--- r_django_essentials/jinja2/extensions.py
+++ r_django_essentials/jinja2/extensions.py
@@ -9,3 +9,11 @@ class I18nExtrasExtension(Extension):
         from django.utils import translation
         g['get_current_language'] = translation.get_language
 
+
+class CryptoExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+        g = environment.globals
+
+        from django.utils.crypto import get_random_string
+        g['get_random_string'] = get_random_string
