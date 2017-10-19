913e930ac2c8445fee1110faa8221ad612c295e5
Jaakko Kantojärvi <jaakko@n-1.fi>
2017-10-19 17:04:13 +0300

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='raphendyr-django-essentials',
-    version='1.0.0',
+    version='1.1.0',
     description='Some essential tools when working with any Django project',
     long_description=long_description,
     keywords='django',
